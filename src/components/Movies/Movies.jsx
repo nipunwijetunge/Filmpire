562e6953323696c15d9cf2baa4de7f484ef0ad00
Nipun Wijetunge <nipunwijetunge@gmail.com>
2024-03-03 10:24:44 +0530

    list movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,13 +8,37 @@ import {
 import { UseSelector } from "react-redux";
 
 import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "..";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data);
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
 
-  return <div>Movies</div>;
+  if (!data.results.length) {
+    return (
+      <Box display="flex" justifyContent="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name. <br /> Please search for something
+          else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return `An error has occurred: ${error}`;
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
